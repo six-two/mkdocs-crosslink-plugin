8013ffe1437da15460f0abb9f361208560d8b2da
six-two <git@six-two.dev>
2023-08-06 19:13:18 +0200

    Profiler: Only show stats when multiple timings exist

--- src/mkdocs_crosslink_plugin/profiling.py
+++ src/mkdocs_crosslink_plugin/profiling.py
@@ -27,21 +27,26 @@ class Profiler:
     
     def log_stats(self):
         for name, time_list in self.timing_map.items():
-            sorted_time = list(sorted(time_list))
             count = len(time_list)
             time_sum = sum(time_list)
-            time_avg = sum(time_list)
-            time_min = sorted_time[0]
-            time_max = sorted_time[-1]
+            message = f"(Profiler) Function '{name}' was called {count} time(s) and took {time_sum:0.2f} seconds."
+            if count > 1:
+                # Statistics (min, max, avg, mean) only make sense when there are multiple values
+                sorted_time = list(sorted(time_list))
+                time_avg = sum(time_list)
+                time_min = sorted_time[0]
+                time_max = sorted_time[-1]
 
-            mid_index = int(count / 2)
-            if count % 2 == 0:
-                # Average of the two middle values
-                time_mean = (sorted_time[mid_index-1] + sorted_time[mid_index]) / 2
-            else:
-                time_mean = sorted_time[mid_index]
+                mid_index = int(count / 2)
+                if count % 2 == 0:
+                    # Average of the two middle values
+                    time_mean = (sorted_time[mid_index-1] + sorted_time[mid_index]) / 2
+                else:
+                    time_mean = sorted_time[mid_index]
 
-            info(f"(Profiler) Function '{name}' was called {count} time(s) and took {time_sum:0.2f} seconds.\nStats: min={ms(time_min)} max={ms(time_max)} avg={ms(time_avg)} mean={ms(time_mean)}")
+                message += f"\nStats: min={ms(time_min)} max={ms(time_max)} avg={ms(time_avg)} mean={ms(time_mean)}"
+
+            info(message)
 
 def ms(time_in_seconds: float) -> str:
     return f"{round(time_in_seconds * 1_000)}ms"
