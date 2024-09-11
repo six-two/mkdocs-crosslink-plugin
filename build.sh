4186319d73a3a6395215145e32513562ddad9fc5
six-two <git@six-two.dev>
2024-09-11 20:14:42 +0200

    Fix: mkdocs not in path

--- build.sh
+++ build.sh
@@ -18,13 +18,13 @@ python3 -m pip install .
 
 # Build main site
 # Needs to be first, since the other files will be copied into it
-mkdocs build || exit 1
+python3 -m mkdocs build || exit 1
 
 build_sub_site() {
     # Switch into directory, build site, copy to the main site, switch back to initial directory
     # Hard assumption: $1 MUST NOT contain a path separator (relative and not nested)
     cd "$1"
-    mkdocs build
+    python3 -m mkdocs build
     cp -r site "../site/$1"
     cd ..
 }
