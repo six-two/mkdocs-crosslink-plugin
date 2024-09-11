9f1a42486093836644ec2e1d96d2f8061c346623
six-two <git@six-two.dev>
2024-09-11 20:11:45 +0200

    Updated dependencies and build script

--- build.sh
+++ build.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
+# Usage: [PORT]
+# Builds the site. If [PORT] is specified, the site will be served on that port
 
 # Change into the project root
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
+
+if [[ -f venv/bin/activate ]]; then
+    echo "[*] Using virtual environment"
+    source venv/bin/activate
+fi
+
+# Install any dependencies
+python3 -m pip install -r requirements.txt
 
 # Install the pip package
 python3 -m pip install .
 
 # Build main site
 # Needs to be first, since the other files will be copied into it
-mkdocs build "$@" || exit 1
+mkdocs build || exit 1
 
 build_sub_site() {
     # Switch into directory, build site, copy to the main site, switch back to initial directory
@@ -23,5 +33,7 @@ build_sub_site site_a
 build_sub_site site_b
 
 # serve site
-cd site
-python3 -m http.server
+if [[ $# -eq 1 ]]; then
+    cd site
+    python3 -m http.server "$1"
+fi
